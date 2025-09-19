6ce66fd71fb101033c43dc7c5d8d7d1b082ca478
LucasBrito <luquinhasbritogba@hotmail.com>
2025-09-19 13:34:26 -0300

    feat(cargo): implementar funcionalidades de gerenciamento de cargos
    - Adicionar páginas para listar, cadastrar e editar cargos
    - Criar componentes de formulário e tabela para manipulação de dados de cargos
    - Implementar store Pinia para gerenciar estado e ações relacionadas a cargos
    - Definir tipos TypeScript para garantir a estrutura dos dados de cargos

---
+++ src/pages/cargo/services/CargoService.ts
@@ -0,0 +1,7 @@
+import ApiService from '@/services/ApiService'
+
+class CargoService extends ApiService {
+  // 👉 methods
+
+}
+export default new CargoService('cargo')
---
+++ src/pages/cargo/types.ts
@@ -0,0 +1,5 @@
+export interface ICargo {
+  nome: string
+  descricao: string
+
+}
